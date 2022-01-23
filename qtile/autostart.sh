0e0375dae4d92d186bffe8323bb64465fc19211c
abdoulayegk <balde9745@outlook.com>
2022-01-23 18:07:22 +0530

    my dotfiles

---
+++ qtile/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#setxkbmap latam
+# picom --experimental-backend --vsync &
+nitrogen --restore &
+volumeicon &
+# nm-applet &
+/usr/bin/nvim
+xargs xwallpaper --stretch < ~/.xwallpaper &
+
+
+
+#numlockx on
+#xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
+#xinput set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
+#amixer set 'Master' 100%
---
+++ qtile/qtile-dependency.md
@@ -0,0 +1,8 @@
+sudo pacman -S brithnessctl
+sudo pacman -S python-docutils
+pip install psutil
+pip install iwlib
+sudo pacman -S alsa-utils
+pip install dbus-next
+sudo pacman -S alsmixer
+sudo pacman -S kitty
